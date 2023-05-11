e4130e5430740e8dbc2c3ff024c3975361a7f334
AarhamH <aarham.haider@gmail.com>
2023-05-10 17:10:02 -0700

    Revert "Added fetchData async funcion and refetch method"
    This reverts commit 0be6f3109b00c53a1984aef051c0856084f6f489.

--- customhooks/useFetch.js
+++ customhooks/useFetch.js
@@ -1,52 +1,8 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import {RAPID_API_KEY} from '@env '
 
-const apiKey = RAPID_API_KEY;
-const useFetch = (endpoint, query) => {
+const useFetch = () => {
     const [data, setData] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
-
-    const options = {
-    method: 'GET',
-    url: `https://api-nba-v1.p.rapidapi.com/${endpoint}`,
-    headers: {
-        'X-RapidAPI-Key': apiKey,
-        'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'
-    },
-    params: {...query},
-    };
-
-    const fetchData = async () => {
-        setIsLoading(true);
-
-        try{
-            const response = await axios.request
-            (options);
-
-            setData(response.data.data);
-            setIsLoading(false);
-        }
-
-        catch(error){
-            setError(error);
-            alert("There is an error");
-        }
-
-        finally{
-            setIsLoading(false);    
-        }
-    }
-
-    useEffect (() => {
-        fetchData();
-    }, []);
-
-    const refetch = () => {
-        setIsLoading(true);
-        fetchData();
-    }
-
-    return { data, isLoading, error, refetch };
 }
